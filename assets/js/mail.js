592ab80f7f60500923da831f36d55a6d6b56acc6
terver timothy <timothy.chahurscelloo.com>
2019-11-28 15:17:20 +0100

    'finished'

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,57 +1,3 @@
-// $(function() {
-
-// 	// Get the form.
-// 	var form = $('#ajax-contact');
-
-// 	// Get the messages div.
-// 	var formMessages = $('#form-messages');
-
-// 	// Set up an event listener for the contact form.
-// 	$(form).submit(function(e) {
-// 		// Stop the browser from submitting the form.
-// 		e.preventDefault();
-
-// 		// Serialize the form data.
-// 		var formData = $(form).serialize();
-
-// 		// Submit the form using AJAX.
-// 		$.ajax({
-// 			type: 'POST',
-// 			url: $(form).attr('action'),
-// 			data: formData
-// 		})
-// 		.done(function(response) {
-// 			// Make sure that the formMessages div has the 'success' class.
-// 			$(formMessages).removeClass('error');
-// 			$(formMessages).addClass('success');
-
-// 			// Set the message text.
-// 			$(formMessages).text(response);
-
-// 			// Clear the form.
-// 			$('#name').val('');
-// 			$('#email').val('');
-// 			$('#company').val('');
-// 			$('#message').val('');
-// 		})
-// 		.fail(function(data) {
-// 			// Make sure that the formMessages div has the 'error' class.
-// 			$(formMessages).removeClass('success');
-// 			$(formMessages).addClass('error');
-
-// 			// Set the message text.
-// 			if (data.responseText !== '') {
-// 				$(formMessages).text(data.responseText);
-// 			} else {
-// 				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-// 			}
-// 		});
-
-// 	});
-
-// });
-
-
 function thefunc() {
 	const email=document.getElementById('mc-email').value; 
 	fetch('https://api-treeprite.herokuapp.com/email', {
@@ -84,12 +30,6 @@ function forcontact(event) {
 	const cemail=document.getElementById('email').value;
 	const ccompany=document.getElementById('company').value;
 	const cmessage=document.getElementById('message').value;
-	console.log(JSON.stringify({
-		name: cname,
-		email: cemail,
-		company: ccompany,
-		message: cmessage,
-	}))
 	fetch('https://api-treeprite.herokuapp.com/contact',{
 	method:"post",
 	body:JSON.stringify({
